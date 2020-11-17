7b6b385a0bee2e9869dbf4eebfa9b95833a9100b
Lino Maurer <lino@q-b.de>
2020-11-17 23:40:04 +0100

    added map and markers - zoom in on load and pop up with info (name, address) on click

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,11 +24,14 @@ require("channels")
 
 // External imports
 import "bootstrap";
-
+import 'mapbox-gl/dist/mapbox-gl.css';
 // Internal imports, e.g:
+import { initMapbox } from './init_mapbox';
 // import { initSelect2 } from '../components/init_select2';
+
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initMapbox();
 });
